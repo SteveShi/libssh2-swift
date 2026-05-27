fba85a9042849de400078e6e9f5e36eb6a1049d5
Steve Shi <xuanlian@mac.com>
2026-05-28 05:20:04 +0800

    Point Package.swift binary target URLs back to v1.0.0 release assets

--- Package.swift
+++ Package.swift
@@ -19,22 +19,22 @@ let package = Package(
         ),
         .binaryTarget(
             name: "libssh2",
-            url: "https://github.com/SteveShi/libssh2-swift/releases/download/v1.0.1/libssh2.xcframework.zip",
+            url: "https://github.com/SteveShi/libssh2-swift/releases/download/v1.0.0/libssh2.xcframework.zip",
             checksum: "25907c36cdf2eb524c0a60c7cde4352cee6dcf2a8b6c819ec7439ec5f38156ba"
         ),
         .binaryTarget(
             name: "libssl",
-            url: "https://github.com/SteveShi/libssh2-swift/releases/download/v1.0.1/libssl.xcframework.zip",
+            url: "https://github.com/SteveShi/libssh2-swift/releases/download/v1.0.0/libssl.xcframework.zip",
             checksum: "c15613e0d7f55e0417ee5cf435ba9d7ff827b16f5839402e7b192c54b761b87b"
         ),
         .binaryTarget(
             name: "libcrypto",
-            url: "https://github.com/SteveShi/libssh2-swift/releases/download/v1.0.1/libcrypto.xcframework.zip",
+            url: "https://github.com/SteveShi/libssh2-swift/releases/download/v1.0.0/libcrypto.xcframework.zip",
             checksum: "4b773d28da70fd45c5c01367fa71288793b536d665caabc54fd3745050cebb60"
         ),
         .binaryTarget(
             name: "libtls",
-            url: "https://github.com/SteveShi/libssh2-swift/releases/download/v1.0.1/libtls.xcframework.zip",
+            url: "https://github.com/SteveShi/libssh2-swift/releases/download/v1.0.0/libtls.xcframework.zip",
             checksum: "4978800009870beb150abd3035313190c6dfa6de833d64da125e25314dd6044c"
         )
     ]
